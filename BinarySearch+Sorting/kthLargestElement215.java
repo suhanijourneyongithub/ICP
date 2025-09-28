3022d789d2896855533abacb587afab6ec4c732f
Suhani Sharma <145221387+suhanijourneyongithub@users.noreply.github.com>
2025-09-28 13:30:59 +0530

    changed the file name

--- BinarySearch+Sorting/kthLargestElement215.java
+++ BinarySearch+Sorting/kthLargestElement215.java
@@ -1,5 +1,5 @@
 import java.util.PriorityQueue;
-public class Solution {
+public class kthLargestElement215 {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         for (int i : nums) {
